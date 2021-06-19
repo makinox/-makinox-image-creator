7e274947c4a359e7ed3af9f3852d565f68e87215
Jesus Bossa <x@jesusbossa.dev>
2021-06-19 16:37:42 -0500

    feat: type creation function

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,5 @@
-import fs from 'fs';
-import { createCanvas, loadImage } from 'canvas';
+import CreateImage from './script/creation';
 
-const width = 1200;
-const height = 630;
+// './src/assets/B.svg'
 
-const canvas = createCanvas(width, height);
-const context = canvas.getContext('2d');
-
-context.fillStyle = '#000';
-context.fillRect(0, 0, width, height);
-
-context.font = 'bold 70pt Open Sans';
-context.textAlign = 'center';
-context.textBaseline = 'top';
-context.fillStyle = '#047788';
-
-const text = 'Hello, World!';
-
-const textWidth = context.measureText(text).width;
-context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120);
-context.fillStyle = '#fff';
-context.fillText(text, 600, 170);
-
-context.fillStyle = '#fff';
-context.font = 'bold 30pt Open sans';
-context.fillText('voib.jesusbossa.dev', 600, 530);
-
-loadImage('./src/assets/B.svg').then((image) => {
-  context.drawImage(image, 20, 530, 65, 85);
-  const buffer = canvas.toBuffer('image/png');
-  fs.writeFileSync('./test.png', buffer);
-});
+CreateImage({ filePath: './test.png' }).then((created) => console.log({ created }));
---
+++ src/script/creation.ts
@@ -0,0 +1,76 @@
+import fs from 'fs';
+import { createCanvas, loadImage } from 'canvas';
+
+interface CreateImageProps {
+  width?: number;
+  height?: number;
+  backgroundColor?: string | CanvasGradient | CanvasPattern;
+  titleFontStyle?: string;
+  textAlign?: CanvasTextAlign;
+  textTitle?: string;
+  titleFillStyle?: string | CanvasGradient | CanvasPattern;
+  titleColor?: string | CanvasGradient | CanvasPattern;
+  subTextColor?: string | CanvasGradient | CanvasPattern;
+  subTextFontStyle?: string;
+  subText?: string;
+  extraLogo?: string;
+  filePath: string;
+}
+
+function CreateImage({
+  width = 1200,
+  height = 630,
+  backgroundColor = '#000',
+  titleFontStyle = 'bold 70pt Open Sans',
+  textAlign = 'center',
+  textTitle = 'Hello, World!',
+  titleFillStyle = '#047788',
+  titleColor = '#fff',
+  subTextColor = '#fff',
+  subTextFontStyle = 'bold 30pt Open sans',
+  subText = 'image-creator.jesusbossa.dev',
+  extraLogo,
+  filePath,
+}: CreateImageProps): Promise<boolean> {
+  return new Promise((resolve) => {
+    try {
+      const canvas = createCanvas(width, height);
+      const context = canvas.getContext('2d');
+
+      context.fillStyle = backgroundColor;
+      context.fillRect(0, 0, width, height);
+
+      context.font = titleFontStyle;
+      context.textAlign = textAlign;
+      context.textBaseline = 'top';
+      context.fillStyle = titleFillStyle;
+
+      const textWidth = context.measureText(textTitle).width;
+      context.fillRect(600 - textWidth / 2 - 10, 170 - 5, textWidth + 20, 120);
+      context.fillStyle = titleColor;
+      context.fillText(textTitle, 600, 170);
+
+      context.fillStyle = subTextColor;
+      context.font = subTextFontStyle;
+      context.fillText(subText, 600, 530);
+
+      if (extraLogo) {
+        loadImage(extraLogo).then((image) => {
+          context.drawImage(image, 20, 530, 65, 85);
+          const buffer = canvas.toBuffer('image/png');
+          fs.writeFileSync(filePath, buffer);
+          resolve(true);
+        });
+      } else {
+        const buffer = canvas.toBuffer('image/png');
+        fs.writeFileSync(filePath, buffer);
+        resolve(true);
+      }
+    } catch (error) {
+      console.error(error);
+      resolve(false);
+    }
+  });
+}
+
+export default CreateImage;
